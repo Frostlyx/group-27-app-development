8c87022a8b03d6bb06b53ec1c38ec69970c0c613
Kevin-Braam <k.j.c.braam@student.tue.nl>
2024-03-15 10:39:03 +0100

    Added buttons to go back and register

--- app/src/main/java/com/example/barcodescanner/ui/login/RegisterCustomerActivity.java
+++ app/src/main/java/com/example/barcodescanner/ui/login/RegisterCustomerActivity.java
@@ -2,15 +2,43 @@ package com.example.barcodescanner.ui.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
+import com.example.barcodescanner.MainActivity;
 import com.example.barcodescanner.R;
+import com.example.barcodescanner.databinding.ActivityRegisterCustomerBinding;
 
 public class RegisterCustomerActivity extends AppCompatActivity {
 
+    private ActivityRegisterCustomerBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register_customer);
+
+        binding=ActivityRegisterCustomerBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        final Button backButton = binding.back;
+        final Button registerButton = binding.register;
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterCustomerActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterCustomerActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/barcodescanner/ui/login/RegisterStoreOwnerActivity.java
+++ app/src/main/java/com/example/barcodescanner/ui/login/RegisterStoreOwnerActivity.java
@@ -2,15 +2,43 @@ package com.example.barcodescanner.ui.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
+import com.example.barcodescanner.MainActivity;
 import com.example.barcodescanner.R;
+import com.example.barcodescanner.databinding.ActivityRegisterStoreOwnerBinding;
 
 public class RegisterStoreOwnerActivity extends AppCompatActivity {
 
+    private ActivityRegisterStoreOwnerBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register_store_owner);
+
+        binding=ActivityRegisterStoreOwnerBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        final Button backButton = binding.back;
+        final Button registerButton = binding.register;
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterStoreOwnerActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterStoreOwnerActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
