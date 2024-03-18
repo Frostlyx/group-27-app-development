51db88106af41e5f0a5c696922ab206935c11b52
Samira1000 <133353375+Samira1000@users.noreply.github.com>
2024-03-18 01:11:33 +0100

    Add list to Store Database

--- app/src/main/java/com/example/barcodescanner/ui/store/StoreActivity.java
+++ app/src/main/java/com/example/barcodescanner/ui/store/StoreActivity.java
@@ -1,6 +1,10 @@
 package com.example.barcodescanner.ui.store;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.barcodescanner.R;
 
 public class StoreActivity extends AppCompatActivity {
 }
