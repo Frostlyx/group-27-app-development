1600efbc7d13cc11f12d08be74890c76b2789ba6
Kevin-Braam <k.j.c.braam@student.tue.nl>
2024-03-18 13:34:49 +0100

    Changed login activities to fragments

--- app/src/main/java/com/example/barcodescanner/ui/login/RegisterStoreOwnerActivity.java
+++ app/src/main/java/com/example/barcodescanner/ui/login/RegisterStoreOwnerActivity.java
@@ -28,7 +28,7 @@ public class RegisterStoreOwnerActivity extends AppCompatActivity {
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(RegisterStoreOwnerActivity.this, LoginActivity.class);
+                Intent intent = new Intent(RegisterStoreOwnerActivity.this, LoginOldActivity.class);
                 startActivity(intent);
             }
         });
