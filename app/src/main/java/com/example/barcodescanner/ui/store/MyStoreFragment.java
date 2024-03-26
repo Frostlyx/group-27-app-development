030d20e25e3a1a60ffd74035f0ae49bea1049e21
Samira1000 <133353375+Samira1000@users.noreply.github.com>
2024-03-26 01:58:16 +0100

    Added tool bar with home button (not functioning for some reason)

--- app/src/main/java/com/example/barcodescanner/ui/store/MyStoreFragment.java
+++ app/src/main/java/com/example/barcodescanner/ui/store/MyStoreFragment.java
@@ -4,11 +4,16 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
+import com.example.barcodescanner.R;
+import com.example.barcodescanner.customer.ChangePasswordFragment;
+import com.example.barcodescanner.customer.MainActivity;
+import com.example.barcodescanner.customer.ProfileFragment;
 import com.example.barcodescanner.databinding.FragmentMystoreBinding;
 
 public class MyStoreFragment extends Fragment {
@@ -32,6 +37,27 @@ public class MyStoreFragment extends Fragment {
         binding.buttonProfile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if (getActivity() != null && getActivity() instanceof StoreActivity) {
+                    ((StoreActivity) getActivity()).replaceFragment(new ProfileFragment());
+                }
+            }
+        });
+
+        binding.buttonItemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (getActivity() != null && getActivity() instanceof StoreActivity) {
+                    ((StoreActivity) getActivity()).replaceFragment(new StoreDatabaseFragment());
+                }
+            }
+        });
+
+        binding.buttonStorePageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (getActivity() != null && getActivity() instanceof StoreActivity) {
+                    ((StoreActivity) getActivity()).replaceFragment(new EditStoreFragment());
+                }
             }
         });
     }
--- app/src/main/java/com/example/barcodescanner/ui/store/StoreActivity.java
+++ app/src/main/java/com/example/barcodescanner/ui/store/StoreActivity.java
@@ -1,10 +1,51 @@
 package com.example.barcodescanner.ui.store;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.example.barcodescanner.R;
+import com.example.barcodescanner.databinding.ActivityStoreBinding;
+import com.example.barcodescanner.ui.login.WelcomeActivity;
 
 public class StoreActivity extends AppCompatActivity {
+
+    ActivityStoreBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityStoreBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        replaceFragment(new MyStoreFragment());
+
+        binding.storeHomeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                replaceFragment(new MyStoreFragment());
+            }
+        });
+
+    }
+
+    public void replaceFragment(Fragment fragment) {
+        setContentView(R.layout.activity_store);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.activity_storeowner_container, fragment);
+        fragmentTransaction.commit();
+    }
+
+    public void replaceActivity() {
+        Intent intent = new Intent(StoreActivity.this, WelcomeActivity.class);
+        startActivity(intent);
+    }
+
 }
