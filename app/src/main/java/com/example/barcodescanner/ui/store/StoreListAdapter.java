990be361fe175b64083625159f9ae8b884d13d3e
Samira1000 <133353375+Samira1000@users.noreply.github.com>
2024-03-31 03:10:18 +0200

    Attempt to implement the Recyclerview for EditStoreFragment.

--- app/src/main/java/com/example/barcodescanner/ui/store/StoreListAdapter.java
+++ app/src/main/java/com/example/barcodescanner/ui/store/StoreListAdapter.java
@@ -3,19 +3,17 @@ package com.example.barcodescanner.ui.store;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.barcodescanner.R;
-import com.example.barcodescanner.customer.Item;
 import com.example.barcodescanner.customer.ProductModel;
 
 import java.util.List;
 
-public class DatabaseListAdapter extends RecyclerView.Adapter<DatabaseListAdapter.ViewHolder> {
+public class StoreListAdapter extends RecyclerView.Adapter<StoreListAdapter.ViewHolder> {
 
     List<ProductModel> itemList;
 
@@ -25,7 +23,7 @@ public class DatabaseListAdapter extends RecyclerView.Adapter<DatabaseListAdapte
      * @param itemList List<Item> containing the data to populate views to be used
      * by RecyclerView
      */
-    public DatabaseListAdapter(List<ProductModel> itemList) {
+    public StoreListAdapter(List<ProductModel> itemList) {
         this.itemList = itemList;
     }
 
@@ -34,7 +32,7 @@ public class DatabaseListAdapter extends RecyclerView.Adapter<DatabaseListAdapte
     public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
         // Create a new view, which defines the UI of the list item
         View view = LayoutInflater.from(viewGroup.getContext())
-                .inflate(R.layout.store_database_row_item, viewGroup, false);
+                .inflate(R.layout.edit_store_column_image, viewGroup, false);
 
         return new ViewHolder(view);
     }
@@ -47,9 +45,6 @@ public class DatabaseListAdapter extends RecyclerView.Adapter<DatabaseListAdapte
         // contents of the view with that element
         ProductModel item = itemList.get(position);
         viewHolder.itemImage.setImageResource(item.getProductImage());
-        viewHolder.itemName.setText(item.getProductName());
-        viewHolder.itemCategory.setText(item.getCategory());
-        viewHolder.itemPrice.setText(item.getProductPrice());
     }
 
     // Return the size of your dataset (invoked by the layout manager)
@@ -64,18 +59,12 @@ public class DatabaseListAdapter extends RecyclerView.Adapter<DatabaseListAdapte
      */
     public static class ViewHolder extends RecyclerView.ViewHolder {
         ImageView itemImage;
-        TextView itemName;
-        TextView itemCategory;
-        TextView itemPrice;
 
         public ViewHolder(View itemView) {
             super(itemView);
             // Define click listener for the ViewHolder's View
 
             itemImage = itemView.findViewById(R.id.image_item);
-            itemName = itemView.findViewById(R.id.text_item_name);
-            itemCategory = itemView.findViewById(R.id.text_item_category);
-            itemPrice = itemView.findViewById(R.id.text_price);
         }
     }
 }
