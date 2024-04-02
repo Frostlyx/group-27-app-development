7f3cbfef3a7df4ff6bf347e3d5cd5f10726ffa4e
Samira1000 <133353375+Samira1000@users.noreply.github.com>
2024-04-02 12:52:23 +0200

    Added and implemented back button for EditProductFragment.

--- app/src/main/java/com/example/barcodescanner/ui/store/EditProductFragment.java
+++ app/src/main/java/com/example/barcodescanner/ui/store/EditProductFragment.java
@@ -49,6 +49,15 @@ public class EditProductFragment extends Fragment {
         binding.storeProductPrice.setText(item.getProductPrice());
         binding.storeProductCategory.setText(item.getCategory());
 
+        binding.buttonBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (view.getContext() != null && view.getContext() instanceof StoreActivity) {
+                    ((StoreActivity) view.getContext()).replaceFragment(new StoreDatabaseFragment(), view.getContext().getString(R.string.edit_product_title));
+                }
+            }
+        });
+
         return binding.getRoot();
 
     }
