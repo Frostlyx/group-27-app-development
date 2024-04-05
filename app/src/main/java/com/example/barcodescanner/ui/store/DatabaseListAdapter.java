9e0407f093d6102c425da0b8f85e8eaa97b4a0b4
Yeet <y.a.eksi@student.tue.nl>
2024-04-05 23:01:28 +0200

    comments

--- app/src/main/java/com/example/barcodescanner/ui/store/DatabaseListAdapter.java
+++ app/src/main/java/com/example/barcodescanner/ui/store/DatabaseListAdapter.java
@@ -1,80 +1,102 @@
 package com.example.barcodescanner.ui.store;
 
-import android.content.Context;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.content.Context; // Importing necessary Android libraries
+import android.view.LayoutInflater; // Importing necessary Android libraries
+import android.view.View; // Importing necessary Android libraries
+import android.view.ViewGroup; // Importing necessary Android libraries
+import android.widget.ImageButton; // Importing necessary Android libraries
+import android.widget.ImageView; // Importing necessary Android libraries
+import android.widget.TextView; // Importing necessary Android libraries
 
-import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.RecyclerView; // Importing necessary Android libraries
 
-import com.example.barcodescanner.R;
-import com.example.barcodescanner.customer.ProductModel;
+import com.example.barcodescanner.R; // Importing custom R class
+import com.example.barcodescanner.customer.ProductModel; // Importing custom ProductModel class
 
-import java.util.ArrayList;
+import java.util.ArrayList; // Importing necessary Java libraries
 
 public class DatabaseListAdapter extends RecyclerView.Adapter<DatabaseListAdapter.ViewHolder> {
 
-    private StoreProductRecyclerViewInterface storeProductRecyclerViewInterface;
-    Context context;
-    StoreProductViewModel storeProductViewModel;
-    ArrayList<ProductModel> productModels;
+    private StoreProductRecyclerViewInterface storeProductRecyclerViewInterface; // Interface for interaction with StoreProductRecyclerView
+    Context context; // Context of the application
+    StoreProductViewModel storeProductViewModel; // ViewModel for StoreProduct
+    ArrayList<ProductModel> productModels; // List of ProductModel objects
 
+    /**
+     * Constructor for DatabaseListAdapter
+     *
+     * @param context Context of the application
+     * @param storeProductRecyclerViewInterface Interface for interaction with StoreProductRecyclerView
+     */
     public DatabaseListAdapter(Context context, StoreProductRecyclerViewInterface storeProductRecyclerViewInterface) {
-        this.context = context;
-        this.storeProductRecyclerViewInterface = storeProductRecyclerViewInterface;
-        this.storeProductViewModel = ((StoreActivity) context).getStoreProductViewModel();
-        this.productModels = storeProductViewModel.getProductModels().getValue();
+        this.context = context; // Initializing context
+        this.storeProductRecyclerViewInterface = storeProductRecyclerViewInterface; // Initializing interface
+        this.storeProductViewModel = ((StoreActivity) context).getStoreProductViewModel(); // Getting ViewModel from StoreActivity
+        this.productModels = storeProductViewModel.getProductModels().getValue(); // Getting list of ProductModels
     }
 
-    // Create new views (invoked by the layout manager)
+    /**
+     * Create new views (invoked by the layout manager)
+     *
+     * @param viewGroup Parent view group
+     * @param viewType Type of view
+     * @return ViewHolder object
+     */
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
-        // Create a new view, which defines the UI of the list item
+        // Inflate the layout for the row item
         View view = LayoutInflater.from(viewGroup.getContext())
                 .inflate(R.layout.store_database_row_item, viewGroup, false);
 
-        return new ViewHolder(view, storeProductRecyclerViewInterface);
+        return new ViewHolder(view, storeProductRecyclerViewInterface); // Return a new ViewHolder
     }
 
-    // Replace the contents of a view (invoked by the layout manager)
+    /**
+     * Replace the contents of a view (invoked by the layout manager)
+     *
+     * @param viewHolder ViewHolder object
+     * @param position Position of the item
+     */
     @Override
     public void onBindViewHolder(ViewHolder viewHolder, final int position) {
-
-        // Get element from your dataset at this position and replace the
-        // contents of the view with that element
+        // Bind data to the views
         ProductModel item = productModels.get(position);
         viewHolder.itemImage.setImageResource(item.getProductImage(0));
         viewHolder.itemName.setText(item.getProductName());
         viewHolder.itemCategory.setText(item.getCategory());
         viewHolder.itemPrice.setText(item.getProductPrice());
-
-    }
-
-    // Return the size of your dataset (invoked by the layout manager)
-    @Override
-    public int getItemCount() {
-        return productModels.size();
     }
 
     /**
-     * Provide a reference to the type of views that you are using
-     * (custom ViewHolder)
+     * Return the size of your dataset (invoked by the layout manager)
+     *
+     * @return Size of the dataset
+     */
+    @Override
+    public int getItemCount() {
+        return productModels.size(); // Return the number of items in the list
+    }
+
+    /**
+     * Custom ViewHolder class
      */
     public static class ViewHolder extends RecyclerView.ViewHolder {
-        ImageView itemImage;
-        TextView itemName;
-        TextView itemCategory;
-        TextView itemPrice;
-        ImageButton buttonEdit;
-        ImageButton buttonBin;
+        ImageView itemImage; // ImageView for item image
+        TextView itemName; // TextView for item name
+        TextView itemCategory; // TextView for item category
+        TextView itemPrice; // TextView for item price
+        ImageButton buttonEdit; // ImageButton for editing
+        ImageButton buttonBin; // ImageButton for deleting
 
+        /**
+         * Constructor for ViewHolder
+         *
+         * @param itemView View for each item
+         * @param storeProductRecyclerViewInterface Interface for interaction with StoreProductRecyclerView
+         */
         public ViewHolder(View itemView, StoreProductRecyclerViewInterface storeProductRecyclerViewInterface) {
             super(itemView);
-            // Define click listener for the ViewHolder's View
-
+            // Initialize views
             itemImage = itemView.findViewById(R.id.image_item);
             itemName = itemView.findViewById(R.id.text_item_name);
             itemCategory = itemView.findViewById(R.id.text_item_category);
@@ -82,12 +104,12 @@ public class DatabaseListAdapter extends RecyclerView.Adapter<DatabaseListAdapte
             buttonEdit = itemView.findViewById(R.id.image_button_edit);
             buttonBin = itemView.findViewById(R.id.image_button_bin);
 
+            // Set click listener for item click
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
                     if (storeProductRecyclerViewInterface != null) {
                         int position = getAdapterPosition();
-
                         if (position != RecyclerView.NO_POSITION) {
                             storeProductRecyclerViewInterface.onItemClick(position);
                         }
@@ -95,12 +117,12 @@ public class DatabaseListAdapter extends RecyclerView.Adapter<DatabaseListAdapte
                 }
             });
 
+            // Set click listener for edit button click
             buttonEdit.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     if (storeProductRecyclerViewInterface != null) {
                         int position = getAdapterPosition();
-
                         if (position != RecyclerView.NO_POSITION) {
                             storeProductRecyclerViewInterface.onEditClick(position);
                         }
@@ -108,12 +130,12 @@ public class DatabaseListAdapter extends RecyclerView.Adapter<DatabaseListAdapte
                 }
             });
 
+            // Set click listener for delete button click
             buttonBin.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
                     if (storeProductRecyclerViewInterface != null) {
                         int position = getAdapterPosition();
-
                         if (position != RecyclerView.NO_POSITION) {
                             storeProductRecyclerViewInterface.onDeleteClick(position);
                         }
--- app/src/main/java/com/example/barcodescanner/ui/store/StoreDatabaseFormState.java
+++ app/src/main/java/com/example/barcodescanner/ui/store/StoreDatabaseFormState.java
@@ -3,47 +3,87 @@ package com.example.barcodescanner.ui.store;
 import androidx.annotation.Nullable;
 
 /**
- * Data validation state of all store forms.
+ * Represents the data validation state of all store forms.
  */
 class StoreDatabaseFormState {
+    // Error messages for each form field
     @Nullable
-    private final Integer nameError;
+    private final Integer nameError; // Error message for the name field
     @Nullable
-    private final Integer barcodeError;
+    private final Integer barcodeError; // Error message for the barcode field
     @Nullable
-    private final Integer amountError;
+    private final Integer amountError; // Error message for the amount field
     @Nullable
-    private final Integer priceError;
+    private final Integer priceError; // Error message for the price field
+
+    // Flag indicating whether all form data is valid
     private final boolean isDataValid;
 
+    /**
+     * Constructs a new StoreDatabaseFormState.
+     *
+     * @param nameError    Error message for the name field, null if no error.
+     * @param barcodeError Error message for the barcode field, null if no error.
+     * @param amountError  Error message for the amount field, null if no error.
+     * @param priceError   Error message for the price field, null if no error.
+     */
     StoreDatabaseFormState(@Nullable Integer nameError, @Nullable Integer barcodeError,
                            @Nullable Integer amountError, @Nullable Integer priceError) {
         this.nameError = nameError;
         this.barcodeError = barcodeError;
         this.amountError = amountError;
         this.priceError = priceError;
+
+        // Set isDataValid to true if all error messages are null, indicating all data is valid
         this.isDataValid = nameError == null && barcodeError == null &&
                 amountError == null && priceError == null;
     }
 
+    /**
+     * Gets the error message for the name field.
+     *
+     * @return The error message for the name field, or null if no error.
+     */
     @Nullable
     Integer getNameError() {
         return nameError;
     }
 
+    /**
+     * Gets the error message for the barcode field.
+     *
+     * @return The error message for the barcode field, or null if no error.
+     */
     @Nullable
     Integer getBarcodeError() {
         return barcodeError;
     }
 
+    /**
+     * Gets the error message for the amount field.
+     *
+     * @return The error message for the amount field, or null if no error.
+     */
     @Nullable
     Integer getAmountError() {
         return amountError;
     }
 
+    /**
+     * Gets the error message for the price field.
+     *
+     * @return The error message for the price field, or null if no error.
+     */
     @Nullable
-    Integer getPriceError() { return priceError; }
+    Integer getPriceError() {
+        return priceError;
+    }
 
+    /**
+     * Checks if all form data is valid.
+     *
+     * @return True if all form data is valid, false otherwise.
+     */
     boolean isDataValid() {
         return isDataValid;
     }
