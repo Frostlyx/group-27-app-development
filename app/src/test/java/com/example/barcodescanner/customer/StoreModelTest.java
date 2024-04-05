e536b6abe878e88b265ce032265db1b0f2db0ba7
The-Eksi <y.a.eksi@student.tue.nl>
2024-04-05 16:06:48 +0200

    test

--- app/src/test/java/com/example/barcodescanner/customer/StoreModelTest.java
+++ app/src/test/java/com/example/barcodescanner/customer/StoreModelTest.java
@@ -1,8 +1,8 @@
 package com.example.barcodescanner.customer;
 
-import static org.junit.Assert.*;
 import com.example.barcodescanner.R;
-import com.example.barcodescanner.customer.StoreModel;
+
+import junit.framework.TestCase;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -10,22 +10,29 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.List;
 
-public class StoreModelTest {
+public class StoreModelTest extends TestCase {
 
     private StoreModel storeModel;
     private List<Integer> expectedImageResId;
+    private List<Integer> expectedImageResId2;
 
-    @Before
+    private int expectedImageResId3 = R.drawable.grocery_app_welcome_icon;
+
     public void setUp() throws Exception {
         // Initialize the list as mutable ArrayList
         expectedImageResId = new ArrayList<>();
         expectedImageResId.add(R.drawable.bread); // ID for bread.jpg in drawable
 
+        expectedImageResId2 = new ArrayList<>();
+        expectedImageResId2.add(R.drawable.grocery_app_welcome_icon); // ID for bread.jpg in drawabl
+
+
+
+
         // Initialize your StoreModel with test data
         storeModel = new StoreModel("Test Store", "123 Test Address", expectedImageResId);
     }
 
-    @Test
     public void testStoreName() {
         // Test getting the store name
         assertEquals("Test Store", storeModel.getStoreName());
@@ -35,8 +42,7 @@ public class StoreModelTest {
         assertEquals("New Test Store", storeModel.getStoreName());
     }
 
-    @Test
-    public void testStoreAddress() {
+    public void testStoreLocation() {
         // Test getting the store address
         assertEquals("123 Test Address", storeModel.getStoreLocation());
 
@@ -44,4 +50,12 @@ public class StoreModelTest {
         storeModel.setStoreLocation("321 New Test Address");
         assertEquals("321 New Test Address", storeModel.getStoreLocation());
     }
+
+    public void testGetStoreImageList() {
+        assertEquals(expectedImageResId, storeModel.getStoreImageList());
+
+        storeModel.setStoreImage(0, expectedImageResId3);
+        assertEquals(expectedImageResId3, storeModel.getStoreImage(0));
+    }
+
 }
