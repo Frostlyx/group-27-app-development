7152a92f9a0c3ee4e9de624f1be3e7d47204f69d
NickFris <n.frissen@student.tue.nl>
2024-04-05 20:22:35 +0200

    Add comments

--- app/src/main/java/com/example/barcodescanner/customer/CategoryRecyclerViewInterface.java
+++ app/src/main/java/com/example/barcodescanner/customer/CategoryRecyclerViewInterface.java
@@ -1,5 +1,8 @@
 package com.example.barcodescanner.customer;
 
+/**
+ * Interface for defining onClickListeners in CategoriesFragment
+ */
 public interface CategoryRecyclerViewInterface {
     void onItemClick(int position);
 }
