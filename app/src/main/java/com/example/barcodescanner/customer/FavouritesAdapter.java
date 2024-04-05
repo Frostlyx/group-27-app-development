b93fa12aba25b5b9994779c99916bf0d5cc6c5e0
NickFris <n.frissen@student.tue.nl>
2024-04-05 20:38:06 +0200

    Added comments, refactored code

--- app/src/main/java/com/example/barcodescanner/customer/FavouritesAdapter.java
+++ app/src/main/java/com/example/barcodescanner/customer/FavouritesAdapter.java
@@ -1,62 +1,71 @@
 package com.example.barcodescanner.customer;
 
 import android.content.Context;
-import android.media.Image;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.barcodescanner.R;
-import com.example.barcodescanner.ui.store.StoreProductRecyclerViewInterface;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
+/**
+ * RecyclerView adapter for displaying a list of favorite products.
+ */
 public class FavouritesAdapter extends RecyclerView.Adapter<FavouritesAdapter.VideoViewHolder> {
 
+    // List of favorite products
     List<ProductModel> itemList;
-    private ProductRecyclerViewInterface productRecyclerViewInterface;
+    // Interface for item click and favorite check events
+    private final ProductRecyclerViewInterface productRecyclerViewInterface;
+    // ViewModel for accessing user's favorite list
     UserListViewModel userListViewModel;
 
+    // Constructor
     public FavouritesAdapter(Context context, List<ProductModel> itemList, ProductRecyclerViewInterface productRecyclerViewInterface) {
         this.itemList = itemList;
         this.productRecyclerViewInterface = productRecyclerViewInterface;
+        // Initialize ViewModel
         this.userListViewModel = ((MainActivity) context).getUserListViewModel();
     }
 
+    // Create new views (invoked by the layout manager)
     @NonNull
     @Override
     public VideoViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        // Inflate the layout for the item
         View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_view, parent, false);
+        // Create a new ViewHolder instance
         return new VideoViewHolder(itemView, productRecyclerViewInterface);
     }
 
+    // Replace the contents of a view (invoked by the layout manager)
     @Override
     public void onBindViewHolder(@NonNull VideoViewHolder holder, int position) {
+        // Get the ProductModel at the current position
+        // Sets its image, name, category
         ProductModel video_item = itemList.get(position);
-        holder.image_view.setImageResource(video_item.getProductImage(0));
-        holder.product_name.setText(video_item.getProductName());
-        holder.bottom_name.setText(video_item.getCategory());
+        holder.imageView.setImageResource(video_item.getProductImage(0));
+        holder.productName.setText(video_item.getProductName());
+        holder.bottomName.setText(video_item.getCategory());
         holder.item = video_item;
         holder.favouritesButton.setChecked(isFavourite(video_item));
     }
 
+    // Return the size of the dataset (invoked by the layout manager)
     @Override
     public int getItemCount() {
         return itemList.size();
     }
 
+    // Method to check if the given product is in the user's favorites list
     private boolean isFavourite(ProductModel item) {
         ArrayList<ProductModel> favouritesList = userListViewModel.getFavouritesList().getValue();
         for (ProductModel pm : favouritesList) {
@@ -67,25 +76,30 @@ public class FavouritesAdapter extends RecyclerView.Adapter<FavouritesAdapter.Vi
         return false;
     }
 
-    public static class VideoViewHolder extends RecyclerView.ViewHolder{
-        ImageView image_view;
-        TextView product_name;
-        TextView bottom_name;
+    // ViewHolder class to hold references to views in the item layout
+    public static class VideoViewHolder extends RecyclerView.ViewHolder {
+        ImageView imageView;
+        TextView productName;
+        TextView bottomName;
         ProductModel item;
         CheckBox favouritesButton;
 
+        // Constructor
         public VideoViewHolder(@NonNull View itemView, ProductRecyclerViewInterface productRecyclerViewInterface) {
             super(itemView);
-            image_view = itemView.findViewById(R.id.image_view);
-            product_name = itemView.findViewById(R.id.product_name);
-            bottom_name = itemView.findViewById(R.id.bottom_name);
+            // Initialize views
+            imageView = itemView.findViewById(R.id.image_view);
+            productName = itemView.findViewById(R.id.product_name);
+            bottomName = itemView.findViewById(R.id.bottom_name);
             favouritesButton = itemView.findViewById(R.id.cbHeart);
+
+            // Set click listener for item view
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     if (productRecyclerViewInterface != null) {
                         int position = getAdapterPosition();
-
+                        // Notify interface of item click event
                         if (position != RecyclerView.NO_POSITION) {
                             productRecyclerViewInterface.onItemClick(position);
                         }
@@ -93,9 +107,11 @@ public class FavouritesAdapter extends RecyclerView.Adapter<FavouritesAdapter.Vi
                 }
             });
 
+            // Set click listener for favorites button
             favouritesButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
+                    // Notify interface of favorite button click event
                     productRecyclerViewInterface.check(item);
                 }
             });
