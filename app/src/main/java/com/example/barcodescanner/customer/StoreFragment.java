6eceaf8140b936a5fdd22b20fb2414ce635f8ee6
Samira1000 <133353375+Samira1000@users.noreply.github.com>
2024-04-05 11:30:06 +0200

    Fixed deletion of user data in realtime database.

---
+++ app/src/main/java/com/example/barcodescanner/customer/StoreFragment.java
@@ -0,0 +1,76 @@
+package com.example.barcodescanner.customer;
+
+import android.app.Dialog;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.barcodescanner.databinding.FragmentStorePageBinding;
+import com.example.barcodescanner.ui.store.ImageListAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StoreFragment extends Fragment {
+
+    StoreModel store;
+    int position;
+    List<Integer> imageList;
+    String storeName;
+    String location;
+
+    private FragmentStorePageBinding binding;
+
+    public StoreFragment(StoreModel store, int position) {
+        this.store = store;
+        this.position = position;
+    }
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = FragmentStorePageBinding.inflate(inflater, container, false);
+
+        imageList = store.getStoreImageList();
+
+        RecyclerView recyclerView = binding.customerRecyclerView;
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        ImageListAdapter imageListAdapter = new ImageListAdapter(imageList);
+        recyclerView.setAdapter(imageListAdapter);
+
+        binding.customerTextviewStorename.setText(store.getStoreName());
+        binding.customerTextviewLocation.setText(store.getStoreLocation());
+
+        return binding.getRoot();
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
+
